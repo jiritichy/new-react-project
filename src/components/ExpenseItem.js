874c11f3093d86317f6f89c2b1196860d5bd0b9f
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-08-13 17:15:29 +0200

    dynamic jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import "./ExpenseItem.css"
 
 const ExpenseItem = () => {
+
+    const expenseDate = new Date(2022, 9, 1)
+    const expenseTitle = "Insurance";
+    const expenseAmount = 150;
+
     return (
         <div className="expense-item">
-            <div>August 7th, 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Phone Bill</h2>
-                <div className="expense-item__price">725CZK</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
             </div>
         </div>
     );
