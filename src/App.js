f28173371e2782ad74b43016135717b2bc4e3038
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-08-13 17:27:26 +0200

    props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
 
-    <ExpenseItem />
+  const expenses = [
+    { id: "1", title: "Insurace", amount: 150, date: new Date(2022, 9, 3) },
+    { id: "2", title: "Food", amount: 180, date: new Date(2022, 9, 2) },
+    { id: "3", title: "Beer", amount: 50, date: new Date(2022, 9, 1) }
+  ]
+
+  return (
+    <>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date.toISOString()} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date.toISOString()} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date.toISOString()} />
+    </>
+
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 
-    const expenseDate = new Date(2022, 9, 1)
-    const expenseTitle = "Insurance";
-    const expenseAmount = 150;
+
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
