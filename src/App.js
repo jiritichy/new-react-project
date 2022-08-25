28158f727ad51c5eaccb86d5eceb58bbeb540580
Tomáš Kravčík <thomas.kravcik@gmail.com>
2022-08-25 20:42:38 +0200

    dynamic filter, but not working properly

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import './App.css';
 import "./components/Expenses/expenses.css"
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpenses/NewExpense';
+import { useState } from 'react';
 
 function App() {
 
-  const expenses = [
+  // const expenses = [
+  //   { id: "1", title: "Insurace", amount: 150, date: new Date(2022, 9, 3) },
+  //   { id: "2", title: "Food", amount: 180, date: new Date(2022, 9, 2) },
+  //   { id: "3", title: "Beer", amount: 50, date: new Date(2022, 9, 1) }
+  // ]
+
+  const [expenses, setExpenses] = useState([
     { id: "1", title: "Insurace", amount: 150, date: new Date(2022, 9, 3) },
     { id: "2", title: "Food", amount: 180, date: new Date(2022, 9, 2) },
     { id: "3", title: "Beer", amount: 50, date: new Date(2022, 9, 1) }
-  ]
+  ])
 
-  //passing expense from child component via pointer
   const addExpenseHandler = (expense) => {
-    console.log(expense)
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    })
   }
 
   return (
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,9 +18,10 @@ function Expenses(props) {
 
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onFilterSelect={filterNewSelect} />
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date} />
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date} />
-            <ExpenseItem title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date} />
+            {
+                props.expenses.map((expense) => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />)
+            }
+
         </Card>
 
     );
